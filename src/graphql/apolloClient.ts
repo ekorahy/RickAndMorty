82e62f67dc6bd52135215c6972364b6a4de2c60b
ekorahy <ekorahy@gmail.com>
2024-09-27 06:18:03 +0700

    fix: change environment variable

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_URI,
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
   cache: new InMemoryCache(),
 })
