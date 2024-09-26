9728db83e34b9e1487603929dee31aa47f480cd7
ekorahy <ekorahy@gmail.com>
2024-09-27 05:52:20 +0700

    style: add styling for item navigation and header scrolled

--- src/components/atoms/NavItem.tsx
+++ src/components/atoms/NavItem.tsx
@@ -1,9 +1,35 @@
-import Link from "next/link";
+"use client";
 
-export default function NavItem({name, route}: NavItemProps) {
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItemProps {
+  name: string;
+  route: string;
+}
+
+export default function NavItem({ name, route }: NavItemProps) {
+  const pathName = usePathname();
+  const isActive = pathName === route;
+
   return (
-    <Link href={route}>
+    <Link
+      className={clsx(
+        "group relative w-max py-2 transition-all duration-300",
+        isActive
+          ? "bg-gradient-to-r from-cyan-400 via-fuchsia-400 to-amber-400 bg-clip-text font-bold text-transparent"
+          : "hover:bg-gradient-to-r hover:from-cyan-400 hover:via-fuchsia-400 hover:to-amber-400 hover:bg-clip-text hover:text-transparent",
+      )}
+      href={route}
+    >
       {name}
+      <span
+        className={clsx(
+          "absolute bottom-0 left-0 h-0.5 rounded-full bg-gradient-to-r from-cyan-400 via-fuchsia-400 to-amber-400 transition-all duration-300",
+          isActive ? "w-full" : "w-0 group-hover:w-full",
+        )}
+      />
     </Link>
   );
 }
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import NavLargeDevice from "./NavLargeDevice";
 import NavSmallDevice from "./NavSmallDevice";
+import clsx from "clsx";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed w-full top-0 z-50">
+    <header
+      className={clsx(
+        "fixed top-0 z-50 w-full transition-all duration-300 ease-in-out",
+        {
+          "bg-slate-950/60 backdrop-blur": scrolled,
+          "bg-transparent": !scrolled,
+        },
+      )}
+    >
       <NavSmallDevice />
       <NavLargeDevice />
     </header>
