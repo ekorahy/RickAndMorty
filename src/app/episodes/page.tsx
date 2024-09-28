a4eba33a107c80ebcc130c7ce75509635a59d70b
ekorahy <ekorahy@gmail.com>
2024-09-29 03:43:32 +0700

    feat(pagination): add pagination for episodes page

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -3,11 +3,14 @@
 import EpisodesSection from "@/components/templates/EpisodesSection";
 import { client } from "@/graphql/apolloClient";
 import { ApolloProvider } from "@apollo/client";
+import { Suspense } from "react";
 
 export default function Episodes() {
   return (
     <ApolloProvider client={client}>
-      <EpisodesSection />
+      <Suspense>
+        <EpisodesSection />
+      </Suspense>
     </ApolloProvider>
   );
 }
