e449cf7e8cfbf9dd2776ec260f4c22e5fd555f30
ekorahy <ekorahy@gmail.com>
2024-10-01 08:04:51 +0700

    style: change method for Logo component from just an image to navigation

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
-  return <Image src="/logo.png" width={60} height={60} alt="Logo image" />;
+  return (
+    <Link href="/">
+      <Image src="/logo.png" width={60} height={60} alt="Logo image" />
+    </Link>
+  );
 }
